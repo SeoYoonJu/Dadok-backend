dfa5a419d5f1d62e32bd5b7fb86a470c0be0afa7
grapyju <yoonju203@naver.com>
2024-12-08 03:26:54 +0900

    feat: add Swagger

---
+++ demo/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("My API").version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+
+    private Info apiInfo(){
+        return new Info()
+                .title("AnyoneToo_BE")
+                .description("구름 단풍톤 Team AnyoneToo_Api_Swagger")
+                .version("1.0.0");
+    }
+}
